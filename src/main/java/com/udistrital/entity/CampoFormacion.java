e039aa92aa02003e4372904ba1f7dbf9c7f8b5e5
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-06-30 01:30:21 -0500

    Se modifica CampoFormacion.java para agregar relación @OneToMany.

--- src/main/java/com/udistrital/entity/CampoFormacion.java
+++ src/main/java/com/udistrital/entity/CampoFormacion.java
@@ -1,11 +1,21 @@
 package com.udistrital.entity;
 
+import java.util.Set;
+import java.util.HashSet;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +26,7 @@ import lombok.experimental.SuperBuilder;
 @Table(name = "campo_formacion")
 @Getter
 @Setter
+@AllArgsConstructor
 @SuperBuilder
 @NoArgsConstructor
 public class CampoFormacion {
@@ -32,5 +43,10 @@ public class CampoFormacion {
 	
 	@Column(name = "cantidad_asignaturas", nullable = true)
     private Integer cantidadAsignaturas;
+	
+	@OneToMany(mappedBy = "campoFormacion", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonIgnore
+	@Builder.Default
+	private Set<AreaFormacion> areasFormacion = new HashSet<>();
 	
 }
