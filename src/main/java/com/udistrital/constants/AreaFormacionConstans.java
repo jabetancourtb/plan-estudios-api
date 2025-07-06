2b413a70de34665a0e89bd97e28a526f07f73490
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-05 21:42:53 -0500

    Se crean clases constants para área y campo de formación.

---
+++ src/main/java/com/udistrital/constants/AreaFormacionConstans.java
@@ -0,0 +1,13 @@
+package com.udistrital.constants;
+
+public class AreaFormacionConstans {
+
+	public static final String ELECTIVA = "Electiva";
+	public static final String ELECTIVAS = "Electivas";
+	public static final String ELECTIVA_CIENCIAS_BASICAS = "Electiva Ciencias Básicas";
+	public static final String ELECTIVA_SOCIO_HUMANISTICA = "Electiva Socio humanística";
+	public static final String ELECTIVA_INEGNEIRIA_APLICADA = "Electiva Ingeniería Aplicada";
+	public static final String ELECTIVA_ECONOMICO_ADMINISTRATIVA = "Electiva Económico Administrativa";
+	public static final String OTROS = "Otros";
+	
+}
---
+++ src/main/java/com/udistrital/constants/CamposFormacionConstants.java
@@ -0,0 +1,11 @@
+package com.udistrital.constants;
+
+public class CamposFormacionConstants {
+
+	public static final String CIENCIAS_BASICAS = "Ciencias Básicas";
+	public static final String SOCIO_HUMANISTICA = "Socio humanística";
+	public static final String INGENIERIA_APLICADA = "Ingeniería Aplicada";
+	public static final String ECONOMICO_ADMINISTRATIVA = "Económico Administrativa";
+	public static final String OTROS = "Otros";
+	
+}
