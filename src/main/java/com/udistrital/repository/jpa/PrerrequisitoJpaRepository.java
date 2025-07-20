c9df950cb6374502ea954a13c74bc88b37c0d396
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-20 16:11:20 -0500

    Refactors PrerrequisitoJpaRepository queries
    Updates PrerrequisitoJpaRepository to use a more accurate and descriptive query structure.
    
    This change addresses previous inaccuracies in how prerequisite relationships were being fetched.
    The query now accurately reflects the relationships between asignaturas.
    Specifically, clarifies which assignments are anterior (prerequisites) and posterior (dependent).

--- src/main/java/com/udistrital/repository/jpa/PrerrequisitoJpaRepository.java
+++ src/main/java/com/udistrital/repository/jpa/PrerrequisitoJpaRepository.java
@@ -11,47 +11,47 @@ import com.udistrital.entity.Prerrequisito;
 public interface PrerrequisitoJpaRepository extends JpaRepository<Prerrequisito, Integer> {
 
 	@Query(value = "SELECT "
-			+ "	   r.id, "
-			+ "	   p.codigo AS prerrequisito_codigo, "
-			+ "    p.semestre_asignatura AS prerrequisito_semestre, "
-			+ "    p.nombre AS prerrequisito, "
-			+ "    a.codigo AS asignatura_codigo, "
-			+ "    a.semestre_asignatura AS asignatura_semestre, "
-			+ "    a.nombre AS asignatura "
-			+ " FROM res_asignatura_prerrequisito r "
-			+ " JOIN res_asignatura a ON r.asignatura_codigo = a.codigo "
-			+ " JOIN res_asignatura p ON r.prerrequisito_codigo = p.codigo", nativeQuery = true)
-	Page<Prerrequisito> findAsignaturasPrerrequisitosWithPaginationAndSorting(PageRequest pageRequest) throws Exception;
+			+ "	   pr.id, "
+			+ "	   a.codigo AS asignatura_anterior_codigo, "
+			+ "    a.semestre_asignatura AS asignatura_anterior_semestre, "
+			+ "    a.nombre AS asignatura_anterior_nombre, "
+			+ "    p.codigo AS asignatura_posterior_codigo, "
+			+ "    p.semestre_asignatura AS asignatura_posterior_semestre, "
+			+ "    p.nombre AS asignatura_posterior_nombre "
+			+ " FROM res_asignatura_prerrequisito pr "
+			+ " JOIN res_asignatura a ON pr.codigo_asignatura_anterior = a.codigo "
+			+ " JOIN res_asignatura p ON pr.codigo_asignatura_posterior = p.codigo", nativeQuery = true)
+	Page<Prerrequisito> findPrerrequisitosWithPaginationAndSorting(PageRequest pageRequest) throws Exception;
 	
 	
 	@Query(value = "SELECT "
-			+ "	   r.id, "
-			+ "	   p.codigo AS prerrequisito_codigo, "
-			+ "    p.semestre_asignatura AS prerrequisito_semestre, "
-			+ "    p.nombre AS prerrequisito, "
-			+ "    a.codigo AS asignatura_codigo, "
-			+ "    a.semestre_asignatura AS asignatura_semestre, "
-			+ "    a.nombre AS asignatura "
-			+ " FROM res_asignatura_prerrequisito r "
-			+ " JOIN res_asignatura a ON r.asignatura_codigo = a.codigo "
-			+ " JOIN res_asignatura p ON r.prerrequisito_codigo = p.codigo"
-			+ " WHERE p.codigo = :codigoPrerrequisito", nativeQuery = true)
-	Page<Prerrequisito> findAsignaturasPosterioresByCodigoPrerrequisitoWithPaginationAndSorting(@Param("codigoPrerrequisito") Integer codigoPrerrequisito, PageRequest pageRequest) throws Exception;
+			+ "	   pr.id, "
+			+ "	   a.codigo AS asignatura_anterior_codigo, "
+			+ "    a.semestre_asignatura AS asignatura_anterior_semestre, "
+			+ "    a.nombre AS asignatura_anterior_nombre, "
+			+ "    p.codigo AS asignatura_posterior_codigo, "
+			+ "    p.semestre_asignatura AS asignatura_posterior_semestre, "
+			+ "    p.nombre AS asignatura_posterior_nombre "
+			+ " FROM res_asignatura_prerrequisito pr "
+			+ " JOIN res_asignatura a ON pr.codigo_asignatura_anterior = a.codigo "
+			+ " JOIN res_asignatura p ON pr.codigo_asignatura_posterior = p.codigo"
+			+ " WHERE p.codigo = :codigo", nativeQuery = true)
+	Page<Prerrequisito> findAsignaturasAnterioresByCodigoAsignaturaWithPaginationAndSorting(@Param("codigo") Integer codigo, PageRequest pageRequest) throws Exception;
 	
 	
 	@Query(value = "SELECT "
-			+ "	   r.id, "
-			+ "	   p.codigo AS prerrequisito_codigo, "
-			+ "    p.semestre_asignatura AS prerrequisito_semestre, "
-			+ "    p.nombre AS prerrequisito, "
-			+ "    a.codigo AS asignatura_codigo, "
-			+ "    a.semestre_asignatura AS asignatura_semestre, "
-			+ "    a.nombre AS asignatura "
-			+ " FROM res_asignatura_prerrequisito r "
-			+ " JOIN res_asignatura a ON r.asignatura_codigo = a.codigo "
-			+ " JOIN res_asignatura p ON r.prerrequisito_codigo = p.codigo"
-			+ " WHERE a.codigo = :codigoAsignatura", nativeQuery = true)
-	Page<Prerrequisito> findAsignaturasPrerrequisitosByCodigoAsignaturaWithPaginationAndSorting(@Param("codigoAsignatura") Integer codigoAsignatura, PageRequest pageRequest) throws Exception;
-	
+			+ "	   pr.id, "
+			+ "	   a.codigo AS asignatura_anterior_codigo, "
+			+ "    a.semestre_asignatura AS asignatura_anterior_semestre, "
+			+ "    a.nombre AS asignatura_anterior_nombre, "
+			+ "    p.codigo AS asignatura_posterior_codigo, "
+			+ "    p.semestre_asignatura AS asignatura_posterior_semestre, "
+			+ "    p.nombre AS asignatura_posterior_nombre "
+			+ " FROM res_asignatura_prerrequisito pr "
+			+ " JOIN res_asignatura a ON pr.codigo_asignatura_anterior = a.codigo "
+			+ " JOIN res_asignatura p ON pr.codigo_asignatura_posterior = p.codigo"
+			+ " WHERE a.codigo = :codigo", nativeQuery = true)
+	Page<Prerrequisito> findAsignaturasPosterioresByCodigoAsignaturaWithPaginationAndSorting(@Param("codigo") Integer codigo, PageRequest pageRequest) throws Exception;
+
 	
 }
